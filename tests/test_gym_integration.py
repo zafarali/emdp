3e67b8959ccfc6ec30318c717ef1d0e57128c0c4
zafarali <zafarali.ahmed@gmail.com>
2018-11-29 19:43:51 -0500

    More tests for return shapes.

--- tests/test_gym_integration.py
+++ tests/test_gym_integration.py
@@ -1,15 +1,17 @@
+import numpy as np
 import gym
 import emdp.emdp_gym
+from emdp import examples
 
 def test_gym_registered():
     gym.make('sb-example3-v0')
     gym.make('two-state-v0')
 
 def check_gym_step(env):
-    env.step(env.action_space.sample())
+    assert env.step(env.action_space.sample()) is not None
 
 def check_gym_reset(env):
-    env.reset()
+    assert env.reset() is not None
 
 
 def test_gym_step():
@@ -23,3 +25,19 @@ def test_gym_reset():
     check_gym_reset(env)
     env = gym.make('two-state-v0')
     check_gym_reset(env)
+
+def test_gym_onehot_observation():
+    env = gym.make('sb-example3-v0')
+    state = env.reset()
+
+    assert isinstance(state, np.ndarray)
+    assert len(state.shape) == 1
+    assert state.shape == (5*5, )
+    # assert state.dtype == np.int8 Breaking change for a future version.
+
+def test_gym_int_observation():
+    env = emdp.emdp_gym.gymify(
+        examples.build_SB_example35(),
+        observation_one_hot=False)
+    state = env.reset()
+    assert type(state) == np.int8
