cb90d24bcb7b4a5f99191adcf2ff46b4fc4e165c
zafarali <zafarali.ahmed@gmail.com>
2019-04-01 11:13:35 -0400

    Add Counter example from ACE paper.

---
+++ emdp/examples/counter.py
@@ -0,0 +1,34 @@
+import numpy as np
+from emdp.common import MDP
+
+def build_imani_counterexample():
+    """
+    MDP counter example given in Fig 1a of Imani, et al.
+    "An Off-policy Policy Gradient Theorem Using Emphatic Weightings."
+    Neurips 2018
+    :return:
+    """
+    # |S| = 4, |A| = 2
+    STATES = 4
+    ACTIONS = 2
+    P = np.zeros((STATES, ACTIONS, STATES))
+    P[0, 0] = [0, 1, 0, 0]
+    P[0, 1] = [0, 0, 1, 0]
+    P[1, 0] = [0, 0, 0, 1]
+    P[1, 1] = [0, 0, 0, 1]
+    P[2, 0] = [0, 0, 0, 1]
+    P[2, 1] = [0, 0, 0, 1]
+    P[3, 0] = [0, 0, 0, 1]
+    P[3, 1] = [0, 0, 0, 1]
+    gamma = 1.0
+    R = np.zeros((STATES, ACTIONS))
+    R[0, 0] = 0
+    R[0, 1] = 0
+    R[1, 0] = 2
+    R[1, 1] = 0
+    R[2, 0] = 0
+    R[2, 1] = 1
+
+    return MDP(P, R, gamma, p0=np.array([1, 0, 0, 0]), terminal_states=[3])
+    pass
+
