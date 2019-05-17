163a6caab2799f1045a87052c296f2ece91b1f5f
zafarali <zafarali.ahmed@gmail.com>
2019-05-17 12:36:08 -0400

    Check if output of function is differentiable.

--- tests/test_torch_analytic.py
+++ tests/test_torch_analytic.py
@@ -10,6 +10,7 @@ except ImportError:
     torch_imported = False
 
 def test_V_pi():
+    """Check if computation works."""
     if not torch_imported:
         return
     mdp = build_SB_example35()
@@ -25,4 +26,21 @@ def test_V_pi():
                                        0.1, 0.7, 0.7, 0.4, -0.4,
                                        -1.0, -0.4, -0.4, -0.6, -1.2,
                                        -1.9, -1.3, -1.2, -1.4, -2.0]))
+
+def test_differentiable():
+    if not torch_imported:
+        return
+
+    mdp = build_SB_example35()
+
+    print(mdp.R)
+    # random policy:
+    policy = np.ones((mdp.P.shape[0], mdp.P.shape[1]))/mdp.P.shape[1]
+    policy = torch.tensor(policy, requires_grad=True).float()
+    V_pi = calculate_V_pi(mdp.P, mdp.R, policy, mdp.gamma)
+    grads = torch.autograd.grad(V_pi.mean(), [policy])
+    assert grads is not None
+    for grad in grads:
+        assert torch.isfinite(grad).all()
+        assert not torch.equal(grad, torch.tensor(0.0))
 
