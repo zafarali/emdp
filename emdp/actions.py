2febcb0778d77fe73bafc222c7c482f628682fb0
zafarali <6295292+zafarali@users.noreply.github.com>
2020-01-31 02:32:20 -0500

    Change integer actions to Enums.

--- emdp/actions.py
+++ emdp/actions.py
@@ -1,4 +1,12 @@
-LEFT = 0
-RIGHT = 1
-UP = 2
-DOWN = 3
+import enum
+
+class Actions(enum.IntEnum):
+    LEFT = 0
+    RIGHT = 1
+    UP = 2
+    DOWN = 3
+
+LEFT = Actions.LEFT
+RIGHT = Actions.RIGHT
+UP = Actions.UP
+DOWN = Actions.DOWN
